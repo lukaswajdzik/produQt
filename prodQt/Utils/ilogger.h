bc8f6abb38373f58b0e987b6f905f0c502f5303a
Krzysztof Reczek <reczek.krzysztof@gmail.com>
2015-06-04 18:34:11 +0200

    K: ILogger

--- prodQt/Utils/ilogger.h
+++ prodQt/Utils/ilogger.h
@@ -1,11 +1,14 @@
 #ifndef ILOGGER_H
 #define ILOGGER_H
 
+#include <string.h>
 
 class ILogger
 {
+//wide accesible singleton
 public:
-    ILogger();
+    virtual void log(string message);
+    virtual ~ILogger;
 };
 
 #endif // ILOGGER_H
