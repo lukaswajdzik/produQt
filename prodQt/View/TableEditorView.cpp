d522ac8239caaa84d46dcf09d64e7ba14c022ff0
lukas.wajdzik@gmail.com <lukas.wajdzik@gmial.com>
2015-12-02 23:31:57 +0100

    table edit part

--- prodQt/View/TableEditorView.cpp
+++ prodQt/View/TableEditorView.cpp
@@ -4,42 +4,59 @@
 #include "TableEditorView.h"
 #include "View/MainWindowView.h"
 
+//QItemSelectionModel
 TableEditorView::TableEditorView(const QString tableName, MainWindowView* p_mainWindow)
     : QWidget(p_mainWindow),
       m_mainWindow(p_mainWindow)
 {
+    m_view = new QTableView;
+    QHBoxLayout *mainLayout = new QHBoxLayout;
+    sutupModel(tableName);
+    m_view->setModel(m_model);
+    m_view->setEditTriggers(QAbstractItemView::DoubleClicked);
+    m_view->setColumnHidden(0, true);
+
+    setupPushbuttons(mainLayout);
+
+    mainLayout->addWidget(m_view);
+
+    setLayout(mainLayout);
+
+    setWindowTitle(tr("Tabela Produktów"));
+
+    m_selmodel = m_view->selectionModel();
+}
+
+void TableEditorView::setupPushbuttons(QHBoxLayout *p_layout)
+{
+    m_submitButton = new QPushButton(tr("Dodaj"));
+    m_submitButton->setDefault(true);
+    m_revertButton = new QPushButton(tr("Cofnij"));
+    m_removeButton = new QPushButton(tr("Usuń"));
+
+    m_buttonBox = new QDialogButtonBox(Qt::Vertical);
+    m_buttonBox->addButton(m_submitButton, QDialogButtonBox::ActionRole);
+    m_buttonBox->addButton(m_revertButton, QDialogButtonBox::ActionRole);
+    m_buttonBox->addButton(m_removeButton, QDialogButtonBox::ActionRole);
+
+    connect(m_submitButton, SIGNAL(clicked()), this, SLOT(submit()));
+    connect(m_revertButton, SIGNAL(clicked()), m_model, SLOT(revertAll()));
+    connect(m_removeButton, SIGNAL(clicked()), this, SLOT(removeRow()));
+
+    p_layout->addWidget(m_buttonBox);
+}
+
+void TableEditorView::sutupModel(const QString tableName)
+{
     m_model = new QSqlTableModel(this);
     m_model->setTable(tableName);
-    m_model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    m_model->setEditStrategy(QSqlTableModel::OnRowChange);
     m_model->select();
 
     m_model->setHeaderData(1, Qt::Horizontal, tr("Nazwa"));
     m_model->setHeaderData(2, Qt::Horizontal, tr("Cena [PLN]"));
     m_model->setHeaderData(3, Qt::Horizontal, tr("Kategoria"));
     m_model->insertRow(m_model->rowCount());
-
-    m_view = new QTableView;
-    m_view->setModel(m_model);
-//    view->resizeColumnsToContents();
-    m_view->setColumnHidden(0, true);
-
-    m_submitButton = new QPushButton(tr("Dodaj"));
-    m_submitButton->setDefault(true);
-    m_revertButton = new QPushButton(tr("Cofnij"));
-
-    m_buttonBox = new QDialogButtonBox(Qt::Vertical);
-    m_buttonBox->addButton(m_submitButton, QDialogButtonBox::ActionRole);
-    m_buttonBox->addButton(m_revertButton, QDialogButtonBox::ActionRole);
-
-    connect(m_submitButton, SIGNAL(clicked()), this, SLOT(submit()));
-    connect(m_revertButton, SIGNAL(clicked()), m_model, SLOT(revertAll()));
-
-    QHBoxLayout *mainLayout = new QHBoxLayout;
-    mainLayout->addWidget(m_view);
-    mainLayout->addWidget(m_buttonBox);
-    setLayout(mainLayout);
-
-    setWindowTitle(tr("Tabela Produktów"));
 }
 
 QWidget *TableEditorView::getView()
@@ -52,11 +69,19 @@ void TableEditorView::submit()
     m_model->database().transaction();
     if (m_model->submitAll()) {
         m_model->database().commit();
+        m_model->insertRow(m_model->rowCount());
     } else {
         m_model->database().rollback();
-        QMessageBox::warning(this, tr("Cached Table"),
-                             tr("The database reported an error: %1")
-                             .arg(m_model->lastError().text()));
+        m_mainWindow->setUserInfoText("Błąd podczas dodawania, spróbuj ponownie", "red");
     }
-    m_model->insertRow(m_model->rowCount());
+}
+
+void TableEditorView::removeRow()
+{
+    m_model->setEditStrategy(QSqlTableModel::OnRowChange);
+    m_model->removeRows(m_selmodel->currentIndex().row(), 1);
+    QString abc = QString("WARTOŚĆ : %1").arg(m_selmodel->currentIndex().row());
+    m_mainWindow->setUserInfoText(abc);
+    m_model->select();
+    m_model->setEditStrategy(QSqlTableModel::OnManualSubmit);
 }
--- prodQt/View/TableEditorView.h
+++ prodQt/View/TableEditorView.h
@@ -9,6 +9,8 @@ class QPushButton;
 class QSqlTableModel;
 class QTableView;
 class MainWindowView;
+class QHBoxLayout;
+class QItemSelectionModel;
 
 class TableEditorView : public QWidget,
                         public IWorkingWindow
@@ -20,14 +22,19 @@ public:
 
 private slots:
     void submit();
+    void removeRow();
+    void sutupModel(const QString tableName);
+    void setupPushbuttons(QHBoxLayout* p_layout);
 
 private:
     MainWindowView *m_mainWindow;
     QPushButton *m_submitButton;
     QPushButton *m_revertButton;
+    QPushButton *m_removeButton;
     QDialogButtonBox *m_buttonBox;
     QSqlTableModel *m_model;
     QTableView *m_view;
+    QItemSelectionModel *m_selmodel;
 };
 
 #endif
